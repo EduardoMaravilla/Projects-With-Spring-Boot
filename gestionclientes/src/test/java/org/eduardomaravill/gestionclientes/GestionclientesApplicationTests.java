a9e4cfaa4455942fd69a11d22147d2fb6a55a316
Eduardo Maravilla <eduardomaravillag@gmail.com>
2024-05-27 17:12:56 -0600

    Create Gestion Clientes

---
+++ gestionclientes/src/test/java/org/eduardomaravill/gestionclientes/GestionclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eduardomaravill.gestionclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
